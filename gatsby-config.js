ceaff8e2032fcf220c99193de25bdfbc765a744a
dwyfrequency <jdwy215@gmail.com>
2019-07-04 22:26:58 -0400

    adding change to header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Material UI Starter`,
-    description: `Kick off your next, great Gatsby project with this Material UI starter. This barebones starter ships with the main Gatsby and Material UI configuration files you might need.`,
-    author: `@dominicabela`,
+    title: `qlStico`,
+    description: `qlStico documentation and marketing materials`,
+    author: `qlStico Contributors`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
